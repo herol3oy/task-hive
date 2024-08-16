086a6ac4096e9cf34304a5b2f07c2a48541b4dc9
Hamed <hamedsedighi@gmail.com>
2024-08-17 01:53:15 +0200

    add tab bar navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,38 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Tabs } from "expo-router";
+import { theme } from "../theme";
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="list" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="clockcircleo" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome5 name="lightbulb" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
+import { Link } from "expo-router";
 import { StyleSheet, View } from "react-native";
-import { ShoppingList } from "./components/ShoppingList";
-import { theme } from "./theme";
+import { ShoppingList } from "../components/ShoppingList";
+import { theme } from "../theme";
 
 export default function App() {
   return (
